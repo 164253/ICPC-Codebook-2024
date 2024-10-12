689bffe9bd814a11fb9a226f8dd3d162d1e01bb0
Rota1001 <johnny1001s000602@gmail.com>
2024-10-12 17:17:24 +0800

    ntt and gaussian

---
+++ nacl/Math/fft.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+inline int read() {
+  int ans = 0;
+  char c = getchar();
+  while (!isdigit(c))
+    c = getchar();
+  while (isdigit(c)) {
+    ans = ans * 10 + c - '0';
+    c = getchar();
+  }
+  return ans;
+}
+typedef complex<double> comp;
+const int MAXN = 1000005;
+const comp I(0, 1);
+const double PI = acos(-1);
+comp A[MAXN * 3], B[MAXN * 3], tmp[MAXN * 3], ans[MAXN * 3];
+void fft(comp F[], int N, int sgn = 1) {
+  if (N == 1)
+    return;
+  memcpy(tmp, F, sizeof(comp) * N);
+  for (int i = 0; i < N; i++)
+    *(i % 2 ? F + i / 2 + N / 2 : F + i / 2) = tmp[i];
+  fft(F, N / 2, sgn), fft(F + N / 2, N / 2, sgn);
+  comp *G = F, *H = F + N / 2;
+  comp cur = 1, step = exp(2 * PI / N * sgn * I);
+  for (int k = 0; k < N / 2; k++) {
+    tmp[k] = G[k] + cur * H[k];
+    tmp[k + N / 2] = G[k] - cur * H[k];
+    cur *= step;
+  }
+  memcpy(F, tmp, sizeof(comp) * N);
+}
+int main() {
+  int n = read(), m = read(), N = 1 << __lg(n + m + 1) + 1;
+  for (int i = 0; i <= n; ++i)
+    A[i] = read();
+  for (int i = 0; i <= m; ++i)
+    B[i] = read();
+  fft(A, N), fft(B, N);
+  for (int i = 0; i < N; ++i)
+    ans[i] = A[i] * B[i];
+  fft(ans, N, -1);
+  for (int i = 0; i <= n + m; ++i)
+    printf("%d ", int(ans[i].real() / N + 0.1));
+  return 0;
+}
---
+++ nacl/Math/ntt.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+const int MAXN = 1000005;
+const int MOD = 998244353, G = 3;
+int rev[MAXN * 3];
+
+int qpow(int x, int y) {
+  int ret = 1;
+  while (y) {
+    if (y & 1) {
+      ret *= x;
+      ret %= MOD;
+    }
+    x *= x;
+    x %= MOD;
+    y >>= 1;
+  }
+  return ret;
+}
+
+void ntt(int F[], int N, int sgn) {
+  int bit = __lg(N);
+  for (int i = 0; i < N; ++i) {
+    rev[i] = (rev[i >> 1] >> 1) | ((i & 1) << (bit - 1));
+    if (i < rev[i])
+      swap(F[i], F[rev[i]]);
+  }
+  for (int l = 1, t = 1; l < N; l <<= 1, t++) {
+    int step = qpow(G, ((MOD - 1) >> t) * sgn + MOD - 1);
+    for (int i = 0; i < N; i += l << 1)
+      for (int k = i, cur = 1; k < i + l; ++k) {
+        int g = F[k], h = (ll)F[k + l] * cur % MOD;
+        F[k] = (g + h) % MOD;
+        F[k + l] = ((g - h) % MOD + MOD) % MOD;
+        cur = (ll)cur * step % MOD;
+      }
+  }
+  if (sgn == -1) {
+    int invN = qpow(N, MOD - 2);
+    for (int i = 0; i < N; ++i)
+      F[i] = (ll)F[i] * invN % MOD;
+  }
+}
